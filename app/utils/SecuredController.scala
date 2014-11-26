04a35d296fc11a70e36aee14d913165baf0271af
Emmanuel Nhan <nhanmanu@gmail.com>
2014-11-26 23:02:49 +0100

    Flexibility on Authorization headers

--- app/utils/SecuredController.scala
+++ app/utils/SecuredController.scala
@@ -13,32 +13,30 @@ import play.api.libs.concurrent.Execution.Implicits._
 /**
  * @author Emmanuel Nhan
  */
-trait SecuredController extends Controller{
+trait SecuredController extends Controller {
 
   object AdminSecuredAction extends ActionBuilder[Request] {
     override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
       val authorization = request.headers.get("Authorization")
       authorization.map { header =>
-        if (header.startsWith("Basic")) {
-          val decoded = new String(Base64.decodeBase64(header.substring(6)))
-          val twoPartsArray = decoded.split(":")
 
-          if (twoPartsArray.length != 2) {
-            Future.successful(Forbidden)
-          } else {
-            val login = twoPartsArray(0)
-            val password = twoPartsArray(1)
-            if (login.equalsIgnoreCase("admin") && password.equalsIgnoreCase("lvsslpdlba")){
-              block(request)
-            } else {
-              Future.successful(Forbidden)
-            }
-          }
+        val decoded = new String(Base64.decodeBase64(header.substring(6)))
+        val twoPartsArray = decoded.split(":")
 
-        }else{
+        if (twoPartsArray.length != 2) {
           Future.successful(Forbidden)
+        } else {
+          val login = twoPartsArray(0)
+          val password = twoPartsArray(1)
+          if (login.equalsIgnoreCase("admin") && password.equalsIgnoreCase("lvsslpdlba")) {
+            block(request)
+          } else {
+            Future.successful(Forbidden)
+          }
         }
-      }.getOrElse{
+
+
+      }.getOrElse {
         Future.successful(Forbidden)
       }
     }
